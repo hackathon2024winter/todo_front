9d45a3919f5b412fba5b6cfa3b0f1b7c2efc560a
tanaka-naoki-heavisidelab <tanakanaoki@heavisidelab.jp>
2024-01-23 08:15:56 +0000

    add product list

--- react-app/src/routes/contact.tsx
+++ react-app/src/routes/contact.tsx
@@ -1,6 +1,30 @@
 // import { useLocation } from 'react-router-dom';
 import { useSearchParams } from 'react-router-dom';
 
+const products = [
+  {
+    id: 1,
+    product_name: 'iPhone',
+    price: 1000,
+  },
+  {
+    id: 2,
+    product_name: 'iPad',
+    price: 500,
+  },
+  {
+    id: 3,
+    product_name: 'iPod',
+    price: 40,
+  },
+  {
+    id: 4,
+    product_name: 'MacBook',
+    price: 2000,
+  },
+];
+
+
 function Contact() {
   // const location = useLocation();
   // console.log(location);
@@ -21,9 +45,17 @@ function Contact() {
   return (
     <div>
       <h2 className="text-2xl font-semibold mt-3 mb-3">Contact</h2>
-      <input 
+      <input
         className="border-2 border-gray-300 focus:border-blue-500 rounded-md p-2 outline-none focus:ring-1 focus:ring-blue-500"
-        type="text" onChange={handleChange} value={paramsValue}></input>
+        type="text" onChange={handleChange} value={paramsValue} />
+
+      <ul className="list-disc pl-5 ml-4">
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.product_name}/{product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
