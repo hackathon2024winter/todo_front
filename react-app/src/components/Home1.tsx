d08455a273e96d75a4ceebdf7b3981d83d7d1fd9
Tanaka Naoki <tanakanaoki@heavisidelab.jp>
2024-02-21 23:26:34 +0900

    oonatu-branch

---
+++ react-app/src/components/Home1.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import ColumnListForm from "./Colums";
+
+const Home1: FC = () => {
+    return (
+        <div className="flex">
+            <ColumnListForm />
+        </div>
+    );
+};
+export default Home1;
