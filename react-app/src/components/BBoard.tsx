1e326590e41d6a9ff75fcfd51eddee126ccdb853
Tanaka Naoki <tanakanaoki@heavisidelab.jp>
2024-02-25 18:07:10 +0900

    temp commit

---
+++ react-app/src/components/BBoard.tsx
@@ -0,0 +1,438 @@
+import { ComponentProps, FC, ReactNode, useCallback } from 'react';
+import {
+  DndContext,
+  UniqueIdentifier,
+  useSensor,
+  useSensors,
+  PointerSensor,
+  KeyboardSensor,
+  DragStartEvent,
+  DragEndEvent,
+  closestCenter
+} from '@dnd-kit/core';
+import {
+  SortableContext,
+  sortableKeyboardCoordinates,
+  rectSortingStrategy,
+  useSortable
+} from '@dnd-kit/sortable';
+
+// idを含むインターフェイス
+export interface HasId {
+  id: UniqueIdentifier;
+};
+
+// ドラッグ&ドロップ可能なリストアイテム
+export const MyDraggableList: FC = <T extends HasId>({
+  items,
+  onDragStart,
+  onDragEnd,
+  children
+}: {
+  items: T[];
+  onDragStart: ComponentProps<typeof DndContext>['onDragStart'];
+  onDragEnd: ComponentProps<typeof DndContext>['onDragEnd'];
+  children: ReactNode;
+}) => {
+  // ドラッグ&ドロップする時に許可する入力
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  return (
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragEnd={onDragStart}
+      onDragStart={onDragEnd}
+    >
+      <SortableContext
+        items={items}
+        strategy={rectSortingStrategy}
+      >
+        <ul>
+          {children}
+        </ul>
+      </SortableContext>
+    </DndContext>
+  );
+}
+
+
+
+
+// import { FC, useEffect, useMemo, useState } from "react";
+// import { createPortal } from "react-dom";
+// import { CategoryFormType } from "../utilities/types";
+// import { CardType, CategoryType } from "../utilities/ttyeps";
+// import Category from "./Category";
+// import {
+//   DndContext,
+//   DragEndEvent,
+//   DragOverlay,
+//   DragStartEvent,
+//   PointerSensor,
+//   useSensors,
+//   useSensor,
+//   DragOverEvent,
+// } from "@dnd-kit/core";
+// import { SortableContext, arrayMove } from "@dnd-kit/sortable";
+// import { useForm } from "react-hook-form";
+// import { v4 as uuid } from "uuid";
+// import { BaseURL } from "../utilities/base_url";
+
+// const Board: FC = () => {
+//   const [cards, setCards] = useState<CardType[]>([]);
+//   const [categorys, setCategorys] = useState<CategoryType[]>([]);
+//   const [isCategoryModal, setCategoryModal] = useState<boolean>(false);
+//   const [activeCard, setActiveCard] = useState<null | CardType>(null);
+//   const [activeCategory, setActiveCategory] = useState<null | CategoryType>(
+//     null
+//   );
+
+//   useEffect(() => {
+//     const fetchCategory = async () => {
+//       const response = await fetch(`${BaseURL()}/getcategories`, {
+//         method: "GET",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       });
+
+//       const resData = await response.json();
+//       // 解析したJSONからitems配列を取得します
+//       const items = resData.data;
+
+//       // `categoryMap` はオブジェクトであり、キーに基づいてユニークな値を保持します。
+//       // これは配列と異なり、JSONオブジェクトではキーがユニークであるため、
+//       // 新しい要素を追加する際に "append"（末尾に追加する操作）は不要です。
+//       // 既存のキーに新しい値を割り当てると、自動的にそのキーの値が更新されます。
+//       const categoryMap: { [key: string]: CategoryType } = {};
+
+//       // `items` 配列をforEachでループし、重複がないように `categoryMap` に要素を追加します。
+//       // ここでのキーは `col_id` であり、これがJSONオブジェクトのユニークなプロパティとなります。
+//       items.forEach((item: CategoryType) => {
+//         if (!categoryMap[item.col_id]) {
+//           // ここでの割り当ては、対応するキーが既に存在しない場合にのみ行われます。
+//           // キーが既に存在する場合、新しい割り当ては無視され、値は保持されます。
+//           categoryMap[item.col_id] = {
+//             col_pos: item.col_pos,
+//             col_id: item.col_id,
+//             col_name: item.col_name,
+//             description: item.description,
+//           };
+//         }
+//       });
+
+//       // マップからソートされた配列を作成し、状態を更新
+//       const categories = Object.values(categoryMap).sort(
+//         (a, b) => a.col_pos - b.col_pos
+//       );
+//       setCategorys(categories);
+//     };
+
+//     const fetchCard = async () => {
+//       const response = await fetch(`${BaseURL()}/getcards`, {
+//         method: "GET",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       });
+
+//       const resData = await response.json();
+//       // 解析したJSONからitems配列を取得します
+//       const items = resData.data;
+
+//       const cardMap: { [key: string]: CardType } = {};
+
+//       items.forEach((item: CardType) => {
+//         // カードのMapの重複を除いて更新
+//         if (!cardMap[item.card_id]) {
+//           cardMap[item.card_id] = {
+//             card_pos: item.card_pos,
+//             card_id: item.card_id,
+//             col_id: item.col_id,
+//             card_name: item.card_name,
+//             input_date: item.input_date,
+//             due_date: item.due_date,
+//             color: item.color,
+//             description: item.description,
+//           };
+//         }
+//       });
+
+//       // マップからソートされた配列を作成し、状態を更新
+//       const cards = Object.values(cardMap).sort(
+//         (a, b) => a.card_pos - b.card_pos
+//       );
+//       setCards(cards);
+//     };
+
+//     fetchCategory();
+//     fetchCard();
+//   }, []); // 依存配列を空に設定して、コンポーネントのマウント時にのみ実行
+
+//   const categorysId = useMemo(
+//     () => categorys.map((category) => category.col_id),
+//     [categorys]
+//   );
+
+//   const createCard = (card: CardType) => {
+//     const cardToAdd: CardType = {
+//       card_pos: 0,
+//       card_id: uuid(),
+//       col_id: card.col_id,
+//       card_name: card.card_name,
+//       input_date: card.input_date,
+//       due_date: card.due_date,
+//       color: card.color,
+//       description: card.description,
+//     };
+
+//     setCards([...cards, cardToAdd]);
+//   };
+
+//   const deleteCard = (id: string) => {
+//     const filteredCards = cards.filter((card) => card.card_id !== id);
+//     setCards(filteredCards);
+//   };
+
+//   const createCategory = (col_name: string) => {
+//     const categoryId = uuid();
+
+//     const categoryToAdd: CategoryType = {
+//       col_id: categoryId,
+//       col_name: col_name,
+//       col_pos: categorys.length * 100,
+//       description: "string",
+//     };
+
+//     setCategorys([...categorys, categoryToAdd]);
+//   };
+
+//   const deleteCategory = (id: string | number) => {
+//     const filteredCategorys = categorys.filter(
+//       (category) => category.col_id !== id
+//     );
+//     setCategorys(filteredCategorys);
+
+//     const newCards = cards.filter((card) => card.col_id !== id);
+//     setCards(newCards);
+//   };
+
+//   const categoryModalOpen = () => {
+//     setCategoryModal(true);
+//   };
+
+//   const categoryModalClose = () => {
+//     setCategoryModal(false);
+//   };
+
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors, isDirty, isValid },
+//   } = useForm<CategoryFormType>();
+
+//   const onsubmit = (data: CategoryFormType) => {
+//     const col_name = data.inputData;
+//     createCategory(col_name);
+//   };
+
+//   const sensors = useSensors(
+//     useSensor(PointerSensor, {
+//       activationConstraint: {
+//         distance: 5,
+//       },
+//     })
+//   );
+
+//   const onDragStart = (event: DragStartEvent) => {
+//     if (event.active.data.current?.type === "CategoryType") {
+//       setActiveCategory(event.active.data.current.category);
+//       return;
+//     }
+
+//     if (event.active.data.current?.type === "CardType") {
+//       setActiveCard(event.active.data.current.card);
+//       return;
+//     }
+//   };
+
+//   const onDragEnd = (event: DragEndEvent) => {
+//     const { active, over } = event;
+//     if (!over) return;
+
+//     const activeCategoryId = active.id;
+//     const overCategoryId = over.id;
+
+//     if (activeCategoryId === overCategoryId) return;
+
+//     const activeCategoryIndex = categorys.findIndex(
+//       (category) => category.col_id === activeCategoryId
+//     );
+
+//     const overCategoryIndex = categorys.findIndex(
+//       (category) => category.col_id === overCategoryId
+//     );
+
+//     // if (overCategoryIndex === 0) return;
+//     // if (overCategoryIndex === categorys.length -1) return;
+
+//     // const activeCategory = categorys[activeCategoryIndex];
+
+//     // const beforeOverCategoryIndex = overCategoryIndex - 1;
+
+//     // const afterOverCategoryIndex = overCategoryIndex + 1;
+
+//     // const overCategoryPosition = categorys[overCategoryIndex].col_pos
+
+//     // const beforeOverCategoryPosition = categorys[beforeOverCategoryIndex].col_pos
+
+//     // const afterOverCategoryPosition = categorys[afterOverCategoryIndex].col_pos
+//     // 上から下への移動の場合(active > over) overのインデックスのposとafterのインデックスのposの比較を行う
+//     // 下から上への移動の場合（active < over) oveｒのインデックスのposとbeforeのインデックスのposの比較を行う
+//     setCategorys((categorys) => {
+//       // console.log(categorys)
+
+//       // if (activeCategoryIndex > overCategoryIndex) {
+//       //   activeCategory.col_pos = (overCategoryPosition + beforeOverCategoryPosition) / 2
+//       // } else if (activeCategoryIndex < overCategoryIndex) {
+//       //   activeCategory.col_pos = (overCategoryPosition + afterOverCategoryPosition) / 2
+//       // }
+
+//       return arrayMove(categorys, activeCategoryIndex, overCategoryIndex);
+//     });
+//   };
+
+//   const onDragOver = (event: DragOverEvent) => {
+//     setActiveCard(null);
+//     setActiveCategory(null);
+
+//     const { active, over } = event;
+//     if (!over) return;
+
+//     const activeCardId = active.id;
+//     const overCardId = over.id;
+
+//     const isActiveCard = active.data.current?.type === "CardType";
+//     const isOverCard = over.data.current?.type === "CardType";
+
+//     if (activeCardId === overCardId) return;
+//     if (!isActiveCard) return;
+
+//     if (isActiveCard && isOverCard) {
+//       setCards((cards) => {
+//         const activeCardIndex = cards.findIndex(
+//           (card) => card.card_id === activeCardId
+//         );
+
+//         const overCardIndex = cards.findIndex(
+//           (card) => card.card_id === overCardId
+//         );
+
+//         cards[activeCardIndex].col_id = cards[overCardIndex].col_id;
+
+//         return arrayMove(cards, activeCardIndex, overCardIndex);
+//       });
+//     }
+
+//     const isAcrossCategory = over.data.current?.type === "CategoryType";
+
+//     if (isActiveCard && isAcrossCategory) {
+//       setCards((cards) => {
+//         const activeCardIndex = cards.findIndex(
+//           (card) => card.card_id == activeCardId
+//         );
+
+//         cards[activeCardIndex].col_id = overCardId.toString();
+
+//         return arrayMove(cards, activeCardIndex, activeCardIndex);
+//       });
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <DndContext
+//         onDragStart={onDragStart}
+//         onDragEnd={onDragEnd}
+//         onDragOver={onDragOver}
+//         sensors={sensors}
+//       >
+//         <div className=" m-auto flex flex-row gap-4">
+//           <div className=" flex flex-row grap-4 gap-4">
+//             <SortableContext items={categorysId}>
+//               {categorys.map((category) => (
+//                 <Category
+//                   key={category.col_id}
+//                   category={category}
+//                   deleteCategory={deleteCategory}
+//                   createCard={createCard}
+//                   cards={cards.filter(
+//                     (card) => card.col_id === category.col_id
+//                   )}
+//                   deleteCard={deleteCard}
+//                 />
+//               ))}
+//             </SortableContext>
+//           </div>
+//           <button
+//             onClick={categoryModalOpen}
+//             className="
+//         h-[60px]
+//         border-2 rounded-lg p-2
+//         bg-orange-200 border-orange-400
+//         cursor-pointer shadow-custom active:shadow-none active:scale-95 focus:outline-none
+//         select-none
+//         "
+//           >
+//             カテゴリの追加
+//           </button>
+//         </div>
+//       </DndContext>
+//       {isCategoryModal && (
+//         <div>
+//           <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
+//             <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+//               <form onSubmit={handleSubmit(onsubmit)}>
+//                 <div>
+//                   <label htmlFor="inputData">タイトル</label>
+//                   <br />
+//                   <input
+//                     id="inputData"
+//                     type="text"
+//                     {...register("inputData", {
+//                       required: "タイトルは必須です",
+//                     })}
+//                     className="bg-gray-50 border border-gray-300 text-gray-900 rounded-md px-3 py-1 focus:ring-gray-500 focus:ring-2"
+//                   />
+//                   {errors.inputData && <div>{errors.inputData.message}</div>}
+//                 </div>
+//                 <button
+//                   type="submit"
+//                   className="w-20 px-4 py-2 bg-blue-500 text-white rounded"
+//                   disabled={!isDirty || !isValid}
+//                 >
+//                   送信
+//                 </button>
+//               </form>
+//               <div className="">
+//                 <button
+//                   onClick={categoryModalClose}
+//                   className="w-20 px-4 py-2 bg-blue-500 text-white rounded"
+//                 >
+//                   閉じる
+//                 </button>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+// export default Board;
--- react-app/src/utilities/types.ts
+++ react-app/src/utilities/types.ts
@@ -1,33 +1,33 @@
 
 
 export interface CardType {
-    card_pos: number;
-    card_id: string;
-    col_id: string;
-    card_name: string;
-    input_date: string;
-    due_date: string;
-    color: string;
-    description: string;
+  card_pos: number;
+  card_id: string;
+  col_id: string;
+  card_name: string;
+  input_date: string;
+  due_date: string;
+  color: string;
+  description: string;
 }
 
 export interface CategoryType {
-    col_pos: number;
-    col_id: string;
-    col_name: string;
-    description: string;
+  col_pos: number;
+  col_id: string;
+  col_name: string;
+  description: string;
 }
 
 export type LoginForm = {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 export type SignupForm = {
-    username: string;
-    email: string;
-    password: string;
-    passwordConfirm: string;
+  username: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
 }
 
 export type CardFormType = {
